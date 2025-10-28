9f0b494adf5a01644964269db56660c40ac4aeb5
shlok612 <shlokkatiyar62@gmail.com>
2025-10-28 23:43:39 +0530

    frontend backbone completed

---
+++ frontend/vite-project/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Conversations from "./Conversations.jsx";
+import LogOut from "./LogOut.jsx";
+
+import Conversation from "./Conversation.jsx";
+import SearchInput from "./SearchInput.jsx";
+
+const Sidebar=()=>{
+  return (
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+ 			<SearchInput />
+ 			<div className='divider px-3'></div>
+ 			<Conversations />
+ 			
+      <LogOut />
+ 		</div>
+  )
+}
+
+export default Sidebar;
